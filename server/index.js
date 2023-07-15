716dfc994e04c949633ec2e8a49fd275386009c9
prabhatkr007 <pkr.876550@gmail.com>
2023-07-16 00:23:48 +0530

    resolved cors error for login

--- server/index.js
+++ server/index.js
@@ -6,13 +6,20 @@ const User = require('./models/User');
 const PORT = process.env.PORT;
 const bcrypt = require('bcrypt');
 const salt = bcrypt.genSaltSync(10);
+const secret = process.env.SECRET_KEY;
+
 const app = express();
+const jwt = require('jsonwebtoken');
 
 require('./DB/conn.js');
 
 
 //middleware
-app.use(cors());
+app.use(cors({
+    credentials: true,
+    origin:'http://localhost:3000'
+}));
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -29,9 +36,26 @@ app.post('/register', async (req, res) => {
         console.log(e);
         res.status(400).json(e);
     }
- 
-
 });
+
+app.post('/login', async (req,res) => {
+    const {username, password} = req.body;
+    const userDoc = await User.findOne({username});
+    
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+  
+    if(passOk){
+        //logedin
+        jwt.sign({username,id:userDoc.id},secret,{}, (err,token) => {
+            if(err) throw err;
+            res.cookie('token',token).json('jwt generated');
+        });
+        // req.json();,()
+    }else{
+        res.status(400).json('wrong credentials');
+    }
+})
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at ${PORT}`);
