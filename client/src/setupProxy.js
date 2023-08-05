99aec33e22abcca0e569654b7994c116d567ee37
prabhatkr007 <pkr.876550@gmail.com>
2023-08-05 16:34:12 +0530

    added backend url

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -5,10 +5,6 @@ module.exports = function(app) {
   let targetURL = 'https://blog-backend-ne6c.onrender.com';
 
 
-  if (process.env.REACT_APP_API_URL) {
-    targetURL = process.env.REACT_APP_API_URL;
-  }
-
   app.use(
     '/api',
     createProxyMiddleware({
