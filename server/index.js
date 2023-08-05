6ad6e5fa9e49829e790d4f1cc049c2347791138b
prabhatkr007 <pkr.876550@gmail.com>
2023-08-05 18:12:10 +0530

    deleted somthing

--- server/index.js
+++ server/index.js
@@ -1,22 +1,12 @@
 const express = require('express');
-// const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const apiRoutes = require('./Routes/api');
 const PORT = process.env.PORT;
 const cookieParser = require('cookie-parser');
-
-
-
 const app = express();
 
 require('./DB/conn.js');
-
-//middleware
-// app.use(cors({
-//     credentials: true,
-//     origin:'https://blog-app-nine-rust.vercel.app'
-// }));
 
 app.use(express.json());
 app.use(cookieParser());
